91d5bd540a4dde1ca7f0b2e00a3a4470f5be3772
Yogev <finegoz@gmail.com>
2017-07-13 11:22:23 -0700

    set config files for Devise in Dev and Prod

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,5 +39,9 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  #devise configuration:
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
 end
