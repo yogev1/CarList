6380f9eec8f47b9dd777d393524f40606828b84a
Yogev <finegoz@gmail.com>
2017-07-22 12:36:46 -0700

    back again

--- app/models/car_image.rb
+++ app/models/car_image.rb
@@ -4,16 +4,4 @@ class CarImage < ApplicationRecord
 
 	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-
-  	# configuring AWS
-
-	    storage: :s3,
-	    s3_credentials: {
-	    bucket: ENV.fetch('S3_BUCKET_NAME'),
-	    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-	    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-	    s3_region: ENV.fetch('AWS_REGION'),
-	    
-	  }
-
 end
