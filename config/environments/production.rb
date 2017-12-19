aeebf932c3987afb9e286da87eba48f070033272
Yogev <finegoz@gmail.com>
2017-12-18 21:28:20 -0800

    wakemydyno

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -108,4 +108,12 @@ Rails.application.configure do
     
   }
 }
+
+
+Dynopoker.configure do |config|
+  config.address = 'http://wakemydyno.com'
+#  config.enable = false # default is true
+#  config.poke_frequency = 123 # default is 1800s (30min)
+end
+
 end
